427ea86baa49470351a9d79abf43205ee32fc32b
LukeVersus <lvcramos@hotmail.com>
2020-11-05 21:24:56 -0300

    Correção create associado.

--- app/routers/associado.js
+++ app/routers/associado.js
@@ -295,7 +295,8 @@ module.exports = async function (app) {
                 "tpEscolaridade": req.body.tpEscolaridade,
                 "faixaSalario": req.body.faixaSalario,
                 "celular": req.body.celular,
-                "municipio_lotacao": req.body.municipio_lotacao
+                "municipio_lotacao": { "id": req.body.municipio_lotacao
+                }
             },
 
         }, function (error, response, body) {
@@ -651,7 +652,8 @@ module.exports = async function (app) {
                 "tpEscolaridade": req.body.tpEscolaridade,
                 "faixaSalario": req.body.faixaSalario,
                 "celular": req.body.celular,
-                "municipio_lotacao": req.body.municipio_lotacao
+                "municipio_lotacao": { "id": req.body.municipio_lotacao
+                }
             },
 
         }, function (error, response, body) {
